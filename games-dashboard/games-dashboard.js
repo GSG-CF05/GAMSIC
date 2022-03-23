c436c6ceb79085f701d5453d78dd95226e692d16
shamskhodary <shams.khodary@gmail.com>
2022-03-23 17:54:52 +0200

    Whenever the user clicks on the MMORPG list, 10 different random will be shown and replaced by new ones in each click relates #15

--- games-dashboard/games-dashboard.js
+++ games-dashboard/games-dashboard.js
@@ -7,10 +7,12 @@ let container = document.querySelector(".container");
 let listAll = document.querySelector(".all");
 let listStrategies = document.querySelector(".strategies");
 let listShooter = document.querySelector(".shooter");
+let listMM = document.querySelector(".mmorpg");
 
 /*BUILD ARRAYS FOR CATEGORIES*/
 let strategiesArray = [];
 let shooterArray = [];
+let mmArray = [];
 
 /*FETCH API LINK START*/
 fetch(
@@ -42,6 +44,8 @@ fetch(
         }
       }
     }
+    /*SEARCHTITLE FUNCTION END*/
+
     /* GET RANDOM CARD FROM API START */
     for (let i = 0; i < 10; i++) {
       let randomAll = Math.floor(Math.random() * data.length);
@@ -153,7 +157,41 @@ fetch(
       }
     }
 
-    /* GET SHOOTER FUNCTION START */
+    /* GET SHOOTER FUNCTION END */
+    /* PUSH MMORPG FUNCTION START */
+    function pushmm() {
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].genre === "MMORPG") {
+          mmArray.push(data[i]);
+        }
+      }
+    }
+    pushmm(); //CALLING FUNCTION
+    /*PUSH MMORPG FUNCTION END*/
+
+    /*GET MMORPG FUNCTION START*/
+    listMM.addEventListener("click", getMm);
+
+    function getMm() {
+      for (let i = 0; i < 10; i++) {
+        let randomMm = Math.floor(Math.random() * mmArray.length);
+        let createCard = document.createElement("div");
+        createCard.setAttribute("class", "card");
+        let createImg = document.createElement("img");
+        createImg.src = mmArray[randomMm].thumbnail;
+        createImg.alt = mmArray[randomMm].title;
+        let createBtn = document.createElement("button");
+        createBtn.setAttribute("index", randomMm);
+        createBtn.classList = `btn`;
+        createBtn.textContent = "More";
+        createBtn.setAttribute("data-id", randomMm);
+        createCard.appendChild(createImg);
+        createCard.appendChild(createBtn);
+        container.replaceChild(createCard, container.childNodes[i]);
+      }
+    }
+
+    /* GET MMORPG FUNCTION END */
   })
   .catch(Error);
-/*SEARCHTITLE FUNCTION END*/
+/*FETCH API LINK END*/
